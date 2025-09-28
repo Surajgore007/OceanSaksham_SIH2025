d5bca35c379167b2e1ed6c990c3666f39caafd2f
SurajGore <139204253+Surajgore007@users.noreply.github.com>
2025-09-28 16:04:07 +0530

    Update index.jsx

--- src/pages/official-console/index.jsx
+++ src/pages/official-console/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Icon from '../../components/AppIcon';
+import Icon from '../../components/Appicon';
 import Button from '../../components/ui/Button';
 import Input from '../../components/ui/Input';
 import Header from '../../components/ui/Header';
